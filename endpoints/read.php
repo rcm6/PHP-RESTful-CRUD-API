b9a91554b311ed0d8c5334e7847cb3e8701bb695
Russell Coleman <rootofsilence@gmail.com>
2023-09-04 11:29:21 +0100

    added read endpoint

--- endpoints/read.php
+++ endpoints/read.php
@@ -1,1 +1,39 @@
-read
+<?php
+// Set error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+// Include database config
+include_once './includes/config.php';
+
+/*
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+*/
+
+// Retrieve data from the database
+$sql = "SELECT * FROM items";
+$result = mysqli_query($conn, $sql);
+
+if (!$result) {
+    $response = ['success' => false, 'message' => 'Failed to retrieve data: ' . mysqli_error($conn)];
+} else {
+    $data = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+    $response = ['success' => true, 'data' => $data];
+}
+
+// Set the JSON content type header
+header('Content-Type: application/json');
+
+// Send the JSON response
+echo json_encode($response);
+
+// Close the database connection
+mysqli_close($conn);
+?>
--- index.php
+++ index.php
@@ -4,19 +4,14 @@ $request_uri = $_SERVER['REQUEST_URI'];
 
 // Check if the requested URI matches a specific endpoint
 if ($request_uri === '/PHP-RESTFUL_CRUD_API/create') {
-    // If the URI matches '/PHP-RESTFUL_CRUD_API/create', include the 'create.php' file
     include_once 'endpoints/create.php';
 } elseif ($request_uri === '/PHP-RESTFUL_CRUD_API/read') {
-    // If the URI matches '/PHP-RESTFUL_CRUD_API/read', include the 'read.php' file
     include_once 'endpoints/read.php';
 } elseif ($request_uri === '/PHP-RESTFUL_CRUD_API/update') {
-    // If the URI matches '/PHP-RESTFUL_CRUD_API/update', include the 'update.php' file
     include_once 'endpoints/update.php';
 } elseif ($request_uri === '/PHP-RESTFUL_CRUD_API/delete') {
-    // If the URI matches '/PHP-RESTFUL_CRUD_API/delete', include the 'delete.php' file
     include_once 'endpoints/delete.php';
 } else {
-    // If none of the above conditions are met
     // - Set a 404 HTTP response code
     http_response_code(404);
     // - Output a JSON response indicating that the endpoint was not found
