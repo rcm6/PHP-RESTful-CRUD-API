5b1e6e9db940bf4c836bb9a01d2fc93dda2d384f
Russell Coleman <rootofsilence@gmail.com>
2023-09-04 10:56:08 +0100

    added connection test

--- includes/config.php
+++ includes/config.php
@@ -16,6 +16,30 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+/*
+// Test: test connection with a simple query
+$sql = "SELECT * FROM items";
+$result = mysqli_query($conn, $sql);
+
+// Check if there are any results
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"] . "<br>";
+        echo "items found in the database.";
+    }
+} else {
+    echo "No items found in the database.";
+}
+
+// Check the query result
+if ($result) {
+    echo "MySQL connection is working.";
+} else {
+    echo "MySQL connection error: " . mysqli_error($conn);
+}
+*/
+
 // Close the connection
 mysqli_close($conn);
 ?>
